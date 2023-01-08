2286d012f8111af369f7dc48ead4d7848a6fa59b
Tomas Krnak <tomas@krnak.cz>
2023-01-08 07:58:24 +0700

    clean

--- zmet/config.py
+++ zmet/config.py
@@ -1,14 +1,13 @@
 import yaml
 import sys
 
+self = sys.modules[__name__]
+
 with open("config.yaml", 'r') as stream:
     try:
         _data = yaml.safe_load(stream)
+        for k, v in _data.items():
+            setattr(self, k, v)
+        del _data
     except yaml.YAMLError as exc:
         print(exc)
-
-self = sys.modules[__name__]
-for k, v in _data.items():
-    setattr(self, k, v)
-
-del _data
