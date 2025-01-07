2f366cee79c6790cbe68e850bfcb8efcc2747fac
Tomas Krnak <tomas@krnak.cz>
2025-01-07 05:28:31 +0100

    add shortlink domains

--- zmet/shortlink.py
+++ zmet/shortlink.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, abort, redirect, url_for
 from .keep import keep
+from . import page
 
 shortlink_bp = Blueprint("shortlink", __name__, url_prefix="/s")
 
@@ -10,4 +11,4 @@ def shortlink(name):
     if not notes:
         abort(404)
     else:
-        return redirect(url_for("page.note", id=notes[0].id))
+        return page.note(id=notes[0].id)
